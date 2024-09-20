24e02d58c43a24cfc35d57e49b7291563694ffc0
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-09-19 18:08:50 -0700

    Initial commit first function

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        float radius = 5.5f;
+        float area = CalculateAreaCircle(radius);
+        Console.WriteLine($"The area of the circle is: {area}");
+    }
+    public static float CalculateAreaCircle(float radius)
+    {
+        return (float)(Math.PI * radius * radius);
+    }
+}
